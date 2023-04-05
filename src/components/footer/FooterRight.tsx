e0e439c910a90d75027057bf1256fc5261369df6
faridulhaque <faridulhaquemurshed@gmail.com>
2023-04-06 01:28:22 +0600

    done

--- src/components/footer/FooterRight.tsx
+++ src/components/footer/FooterRight.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import styles from "../../styles/footer/FooterRight.module.css"
-
+import React from "react";
+import styles from "../../styles/footer/FooterRight.module.css";
 
 const FooterRight = () => {
-    return (
-        <div className={styles.main}>
-            <h2>آخر مقال</h2>
+  return (
+    <div className={styles.main}>
+      <h2>آخر مقال</h2>
 
-            <h3>ريال إستيت في <q>Roundup</q>!</h3>
-            <h4>الثلاثاء - 19 أبريل - 10:39 صباحًا</h4>
+      <h3>
+        ريال إستيت في <q>Roundup</q>!
+      </h3>
+      <h4>الثلاثاء - 19 أبريل - 10:39 صباحًا</h4>
 
-            <h3>نحن جاهزون لقواعد TRID!</h3>
-            <h4>الجمعة - 15 أبريل - 5:07 مساءً</h4>
+      <h3>نحن جاهزون لقواعد TRID!</h3>
+      <h4>الجمعة - 15 أبريل - 5:07 مساءً</h4>
 
-            <h3>ريال إستيت في <q>Roundup</q>!</h3>
-            <h4>الثلاثاء - 19 أبريل - 10:39 صباحًا</h4>
+      <h3>
+        ريال إستيت في <q>Roundup</q>!
+      </h3>
+      <h4>الثلاثاء - 19 أبريل - 10:39 صباحًا</h4>
 
-
-        </div>
-    );
+      <button>المزيد من المقالات</button>
+    </div>
+  );
 };
 
 export default FooterRight;
