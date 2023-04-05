d37444c7620de391937cc55fcfa3bee1537b3d68
faridulhaque <faridulhaquemurshed@gmail.com>
2023-04-06 01:21:49 +0600

    footer done

--- src/components/footer/FooterMid.tsx
+++ src/components/footer/FooterMid.tsx
@@ -1,13 +1,40 @@
-import React from 'react';
-import styles from "../../styles/footer/FooterMid.module.css"
-
+import React from "react";
+import styles from "../../styles/footer/FooterMid.module.css";
 
 const FooterMid = () => {
-    return (
-        <div className={styles.main}>
-            
+  return (
+    <div className={styles.main}>
+      {/* contact part started */}
+      <div>
+        <h2>اتصل بنا</h2>
+        <div className={styles.contact}>
+          <p className={styles.virInfo}>
+            +1 202 555 0135
+            <br />
+            +1 202 555 0145
+            <br />
+            contact@realtyspace.com
+          </p>
+
+          <p className={styles.phyInfo}>
+            شارع فرانكلين، نيويورك، نيويورك ١٠٠١٣ الولايات المتحدة الأمريكية من
+            الإثنين إلى الجمعة
+          </p>
         </div>
-    );
+      </div>
+      {/* contact part ended */}
+
+      {/* feedback started */}
+      <div className={styles.feedback}>
+        <h2>تقييم</h2>
+        <input placeholder="البريد الإلكتروني" type="email" />
+        <textarea placeholder="رسالة لنا" name="" id=""></textarea>
+        <button>إرسال</button>
+      </div>
+
+      {/* feedback ended */}
+    </div>
+  );
 };
 
 export default FooterMid;
--- src/components/footer/FooterRight.tsx
+++ src/components/footer/FooterRight.tsx
@@ -5,7 +5,18 @@ import styles from "../../styles/footer/FooterRight.module.css"
 const FooterRight = () => {
     return (
         <div className={styles.main}>
-            
+            <h2>آخر مقال</h2>
+
+            <h3>ريال إستيت في <q>Roundup</q>!</h3>
+            <h4>الثلاثاء - 19 أبريل - 10:39 صباحًا</h4>
+
+            <h3>نحن جاهزون لقواعد TRID!</h3>
+            <h4>الجمعة - 15 أبريل - 5:07 مساءً</h4>
+
+            <h3>ريال إستيت في <q>Roundup</q>!</h3>
+            <h4>الثلاثاء - 19 أبريل - 10:39 صباحًا</h4>
+
+
         </div>
     );
 };
