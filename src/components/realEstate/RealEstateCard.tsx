3c7a1636ef31b07bd731764906efdb794c8dd69a
faridulhaque <faridulhaquemurshed@gmail.com>
2023-04-10 15:01:40 +0600

    hover effect on real estate card done

--- src/components/realEstate/RealEstateCard.tsx
+++ src/components/realEstate/RealEstateCard.tsx
@@ -5,13 +5,28 @@ import Image from "next/image";
 const RealEstateCard = ({ data }: any) => {
   return (
     <div className={styles.realEstateCard}>
-      <Image
-        className={styles.cardImg}
-        src={data.img}
-        alt="home"
-        width={350}
-        height={175}
-      ></Image>
+      <div className={styles.imgContainer}>
+        <Image
+          className={styles.cardImg}
+          src={data.img}
+          alt="home"
+          width={350}
+          height={175}
+        ></Image>
+
+        {/* positionally absolute hover effect on card */}
+        <div className={styles.hoverEffect}>
+          <div>
+            <h3>نوع العقار: كوخ</h3>
+            <h3>نوع العقد: {data.status}</h3>
+            <h3>تاريخ الإضافة: منذ 7 سنوات</h3>
+            <small>هذا نص دامي بسبع كلمات...</small>
+            <p><span>تاريخ الإضافة: منذ 7 سنوات</span> <button>عرض التفاصيل</button></p>
+          </div>
+        </div>
+      </div>
+
+
       <div className={styles.textContainer}>
         <h2>{data.title}</h2>
         <h2>{data.location}</h2>
