f4efda01e28b85c46187996a87352e82df01482b
faridulhaque <faridulhaquemurshed@gmail.com>
2023-04-11 03:10:48 +0600

    new and blog responsive and revise done

--- src/components/mission/MissionCard.tsx
+++ src/components/mission/MissionCard.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import styles from "../../styles/mission/MissionCard.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const MissionCard = ({ mission }: any) => {
+const MissionCard = ({ mission, i}: any) => {
   return (
-    <div className={styles.missionCard}>
-      <FontAwesomeIcon icon={mission.icon} className={styles.icon} />
+    <div className={`${styles.missionCard} ${styles.gridItem + i +1}`}>
+      {/* <FontAwesomeIcon icon={mission.icon} className={styles.icon} /> */}
+
+      <span className={styles.icon}>{mission?.icon}</span>
 
       <h1>{mission.title}</h1>
       <p>{mission.text}</p>
--- src/components/newAndBlog/NewsAndBlogCard.tsx
+++ src/components/newAndBlog/NewsAndBlogCard.tsx
@@ -1,16 +1,22 @@
-import React from 'react';
-import styles from "../../styles/newsAndBlog/NewsAndBlogCard.module.css"
-import Image from 'next/image';
+import React from "react";
+import styles from "../../styles/newsAndBlog/NewsAndBlogCard.module.css";
+import Image from "next/image";
 
-const NewsAndBlogCard = ({blog}:any) => {
-    return (
-        <div className={styles.card}>
-            <Image src={blog.img} width={300} height={400} alt="home"></Image>
-            <h2>{blog.title}</h2>
-            <p>{blog.text}</p>
-            <button>اقرأ المزيد</button>
-        </div>
-    );
+const NewsAndBlogCard = ({ blog }: any) => {
+  return (
+    <div className={styles.card}>
+      <Image src={blog.img} width={300} height={400} alt="home"></Image>
+      <h2>{blog.title}</h2>
+      <p>{blog.text}</p>
+      <button>اقرأ المزيد</button>
+
+      {/* corner div */}
+      <div className={styles.corner}>
+        <span>أبريل</span>
+        <span>7</span>
+      </div>
+    </div>
+  );
 };
 
 export default NewsAndBlogCard;
